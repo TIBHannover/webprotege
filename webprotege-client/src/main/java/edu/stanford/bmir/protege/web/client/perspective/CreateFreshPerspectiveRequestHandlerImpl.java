bf90dad029bfc7547cb3dfc7f4928e209a8674bf
Fawad Khan <tgfawad@users.noreply.github.com>
2023-05-25 18:01:09 +0200

    more debug messages

--- webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/perspective/CreateFreshPerspectiveRequestHandlerImpl.java
+++ webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/perspective/CreateFreshPerspectiveRequestHandlerImpl.java
@@ -1,6 +1,8 @@
 package edu.stanford.bmir.protege.web.client.perspective;
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
 import edu.stanford.bmir.protege.web.client.form.LanguageMapCurrentLocaleMapper;
 import edu.stanford.bmir.protege.web.client.library.msgbox.InputBox;
 import edu.stanford.bmir.protege.web.client.uuid.UuidV4Provider;
@@ -12,7 +14,6 @@ import javax.annotation.Nonnull;
 import javax.inject.Inject;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import com.allen_sauer.gwt.log.client.Log;
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
@@ -50,7 +51,9 @@ public class CreateFreshPerspectiveRequestHandlerImpl implements CreateFreshPers
                 return;
             }
             //Log.debug(" check whats happening here " + uuidV4Provider.get());
-            PerspectiveId perspectiveId = PerspectiveId.get(trimmedInput);
+            String uuid = Document.get().createUniqueId();
+            Log.debug("this is the new Id that was generated: " + uuid);
+            PerspectiveId perspectiveId = PerspectiveId.get(uuid);
             Log.debug("lets see whats the perspective Id " + perspectiveId);
             String langTag = localeMapper.getCurrentLang();
             LanguageMap label = LanguageMap.of(langTag, trimmedInput);
